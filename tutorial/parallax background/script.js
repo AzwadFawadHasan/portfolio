e3ec42173dc0b56316a5c06814deed21d81de962
azwad <azwadfawadhasan@gmail.com>
2022-09-30 14:30:44 +0600

    created layer class with update() and draw() methods

--- tutorial/parallax background/script.js
+++ tutorial/parallax background/script.js
@@ -20,16 +20,46 @@ let z=0;
 let x =0;//serves as horizontal position for one of the background images
 let x2 =2400;//x2 for second identical image
 //2400 is the image width(full)
-function animate(){
+
+
+class Layer{
+    constructor(image, speedModifier){
+        this.x=0;
+        this.y =0;//vertical y coordinate;
+        this.width=2400;
+        this.height=700;
+        this.x2=this.width;
+        this.image=image;
+        this.speedModifier=speedModifier;//controls speed of background image
+        this.speed = gameSpeed * this.speedModifier;
+
+    }
+    update(){//move layers horizontally by changing their this.x and y properties
+        if(this.x <= -this.width){
+            this.x = this.width + this.x2 -this.speed;
+        }
+        if (this.x2 < -this.width){
+            this.x2 = this.width+this.x-this.speed;
+        }
+        this.x=Math.floor(this.x-this.speed);
+        this.x2=Math.floor(this.x2-this.speed);
+    }
+    draw(){//takes info from the layer object and draws it onto the canvas
+        ctx.drawImage(this.image, this.x,this.y,this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+function animate(){//parallax effect is when the foreground layer moves faster than the background layer
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4,x,0);
-    ctx.drawImage(backgroundLayer4,x2,0);
+    //ctx.drawImage(backgroundLayer4,x,0);
+    //ctx.drawImage(backgroundLayer4,x2,0);
     //x2 starts where the first image ends
-    if(x<-2400)x=2400-gameSpeed;//reset check
+    //if(x<-2400)x=2400-gameSpeed;//reset check
     //when x becomes 2400 x2 reduces by 15 pixels
-    else x-=-gameSpeed;
-    if(x2<-2400)x2=2400 -gameSpeed;
-    else x2-= gameSpeed;
+    //else x-=-gameSpeed;
+    //if(x2<-2400)x2=2400 -gameSpeed;
+    //else x2-= gameSpeed;
     requestAnimationFrame(animate);
 
 }
