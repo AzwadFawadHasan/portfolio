7a39ba39843e004b302fb4700e131b8786443803
azwad <azwadfawadhasan@gmail.com>
2022-10-02 12:33:05 +0600

    created folder for collision animation in sprite sheets

---
+++ tutorial/collision animations from sprite sheet/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementsById('canvas1');
+const ctx = canvas;getContext('2d');
+canvas.width = 500;
+canvas.height = 700;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(50,50,100,150);
+
+const explosions =[];
