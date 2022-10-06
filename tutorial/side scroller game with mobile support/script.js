6d343b7ad3e7f0da48c6243a02e2a4e5b947fa20
azwad <azwadfawadhasan@gmail.com>
2022-10-06 21:05:45 +0600

    key press identifier added

--- tutorial/side scroller game with mobile support/script.js
+++ tutorial/side scroller game with mobile support/script.js
@@ -3,7 +3,7 @@
 
 
 
-document.addEventListener('load', function(){//benefit of using an anonymous function
+document.addEventListener('DOMContentLoaded', function(){//benefit of using an anonymous function
     //a function without a name is anonymous function
     //we can sepearte our scope of the game from a global scope 
 
@@ -12,8 +12,65 @@ const ctx = canvas.getContext('2d');// this is the instance of built in canvas 2
 canvas.width = 500;
 canvas.height = 720;
 
+class InputHandler{//puts event listeners  to keyboard events and holds arrays of all active keys
+    constructor(){
+        this.keys = [];//array helps to add and remove key presses
+        window.addEventListener('keydown', e =>{
+            console.log(e.key);
+            if(
+                (e.key === 'ArrowDown' || e.key ==='ArrowUp' || e.key=== 'ArrowLeft' || e.key === 'ArrowRight')
+             && this.keys.indexOf(e.key)=== -1){// this means this.key which is arrow donwn is not present in the array
+                this.keys.push(e.key);
+            }
+            console.log(e.key, this.keys);  
+            //ES6 arrow functions don't bind their own 'this' but
+            // they inherit the one from their parent scope this
+            // this is called lexical scoping
+        });
+
+        window.addEventListener('keyup', e =>{
+            console.log(e.key);
+            if((e.key === 'ArrowDown' || e.key ==='ArrowUp' || e.key=== 'ArrowLeft' || e.key === 'ArrowRight')
+                 && this.keys.indexOf(e.key)=== -1){// this means this.key which is arrow donwn is not present in the array
+                this.keys.splice(this.keys.indexOf(e.key),1);//if key that was RELEASED was arrow down we want to remove it from htre array
+            
+            //splice takes 2 arguments index of key that needs to be removed
+            // and how many elements starting from that index we want to remove
+            //1 means to remove 1 element from tthat array
+            }
+            console.log(e.key, this.keys);  
+          
+        });
+    }
+}
+
+class Player{
+    //player class will react to these keys
+}
+
+class Background{
+    //seperate class to handle scrolling backgrounds 
+}
+
+class Enemy{
+//contains enemy properties
+}
 
 
+function handdleEnemies(){
+    //seperate function to handle enemies,
+    //like adding and removing enemies
+}
+
+function displayStatusText(){
+    //display score and lives
+}
+
+const input = new InputHandler();
+
+function animate(){
+    //UPDATES    and draws enemeies over and over
+}
 
 
 });
