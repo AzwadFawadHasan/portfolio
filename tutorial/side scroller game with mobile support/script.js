7f839dfa56273943838a4dae5706931c73edb9e0
azwad <azwadfawadhasan@gmail.com>
2022-10-06 19:46:32 +0600

    added all classes and functiosn for side scroller game

--- tutorial/side scroller game with mobile support/script.js
+++ tutorial/side scroller game with mobile support/script.js
@@ -3,12 +3,14 @@
 
 
 
-document.addEventListener('load', function(){
+document.addEventListener('load', function(){//benefit of using an anonymous function
+    //a function without a name is anonymous function
+    //we can sepearte our scope of the game from a global scope 
 
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');// this is the instance of built in canvas 2D api that holds all drawing methods and properties we need to animate our game
 canvas.width = 500;
-canvas.height = 700;
+canvas.height = 720;
 
 
 
